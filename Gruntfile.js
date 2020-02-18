c2d5e4d505bf44df7a7ccc2a4a299669f030ebc2
Michael Tamaki <themichaeltamaki@gmail.com>
2020-02-17 21:27:07 -0800

    Version 2 without framework

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+const sass = require('node-sass');
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserSync: {
+      bsFiles: {
+        src: 'dist/*'
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: './dist'
+        }
+      }
+    },
+    clean: ['dist/'],
+    copy: {
+      main: {
+        src: 'node_modules/normalize.css/normalize.css',
+        dest: 'dist/normalize.css'
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          removeComments: true
+        },
+        files: {
+          'dist/index.html': 'src/html/index.html',
+        }
+      }
+    },
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: true,
+        outputStyle: 'compressed'
+      },
+      dist: {
+        files: {
+          'dist/index.css': 'src/scss/index.scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: ['src/html/index.html'],
+        tasks: ['htmlmin']
+      },
+      style: {
+        files: ['src/scss/index.scss'],
+        tasks: ['sass']
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'copy', 'htmlmin', 'sass', 'browserSync', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# Michael Tamaki Personal Website
-The website serves as a personal branding tool to give more details of the projects and experiences that I've been a part of. The blog is powered by Jekyll, which allows for easy integration with Github Pages.
-- Start a local development server: `jekyll serve`  
-- Build html pages from markdown posts: `jekyll build`  
+# michaeltamaki.github.io
+
+## Install dependencies
+
+```
+npm install
+```
+
+## Watch `src` for changes and start browser sync
+
+```
+npm run serve
+```
