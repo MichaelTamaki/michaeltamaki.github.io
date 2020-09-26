c25ef40a8a637fcd26a04d26c0b38d023d7e1d7f
Michael Tamaki <themichaeltamaki@gmail.com>
2020-09-26 13:38:21 -0700

    Use full tailwindcss class so it does not get purged

--- src/components/util/SocialLink.js
+++ src/components/util/SocialLink.js
@@ -2,10 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 
 function SocialLink({ href, Icon, text, isLightText }) {
-  const textColorGrade = isLightText ? "400" : "800"
   return (
     <a
-      className={`hover:text-green-${textColorGrade} focus:text-green-${textColorGrade} transition-all duration-150 ease-linear`}
+      className={`${isLightText ? 'hover:text-green-400 focus:text-green-400' : 'hover:text-green-800 focus:text-green-800'}
+      transition-all duration-150 ease-linear`}
       href={href}
       target="_blank"
       rel="noreferrer"
