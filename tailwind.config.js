11da6f710fa3d07f657089350e71f39b66813b54
Michael Tamaki <michaeltamaki@pm.me>
2023-08-08 20:50:21 +0000

    apply prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+  ],
   theme: {
     extend: {},
   },
